6994e9dac4af6c5946142a26ab0dc38990c4738a
Oleh <olkozhushko@gmail.com>
2019-05-24 16:35:21 +0300

    made build and minor changes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const favicon = require('express-favicon');
+
+
+const app = express();
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
